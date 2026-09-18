9755684c87c4a4b6b79b95b7747741e5bf419242
Yitzak Emile <codemanhh78@gmail.com>
2026-09-18 18:41:28 -0500

    roles aplicados, conexion bd correcta

--- src/frontend/src/obj/Debug/net9.0/src.AssemblyInfo.cs
+++ src/frontend/src/obj/Debug/net9.0/src.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("src")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+63a3137b5f38605cc301d506494c68d153de600b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3b48cdecea7477bfba99b595a271a470517e4728")]
 [assembly: System.Reflection.AssemblyProductAttribute("src")]
 [assembly: System.Reflection.AssemblyTitleAttribute("src")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
